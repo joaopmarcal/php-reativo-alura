4c059f92f19938d04e6beb9133d5b76aae446fcb
João Paulo <joaopaulozangrande@gmail.com>
2021-09-13 15:37:10 -0300

    function stream_select

--- io-nb.php
+++ io-nb.php
@@ -9,4 +9,20 @@ foreach ($streamList as $stream) {
     stream_set_blocking($stream, false);
 }
 
-echo fgets($streamList[0]);
+do {
+
+    $copyReadStream = $streamList;
+    $numeroDeStreams = stream_select($copyReadStream, $write, $except, 0, 200000);
+
+    if($numeroDeStreams === 0) {
+        continue;
+    }
+
+    foreach($copyReadStream as $key => $stream){
+        echo fgets($stream);
+        unset($streamList[$key]);
+    }
+
+} while (!empty($streamList));
+
+echo "Li todos os arquivos" . PHP_EOL;
