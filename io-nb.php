385e2c8b5f5d002178cf67d29d46317585ecb077
João Paulo <joaopaulozangrande@gmail.com>
2021-09-13 17:24:11 -0300

    creating a server

--- io-nb.php
+++ io-nb.php
@@ -1,10 +1,12 @@
 <?php
 
 $streamList = [
+    stream_socket_client('tcp://localhost:8080'),
     fopen('file.txt','r'),
     fopen('file2.txt','r'),
 ];
 
+fwrite($streamList[0], 'GET /http-server.php HTTP/1.1' . PHP_EOL . PHP_EOL);
 foreach ($streamList as $stream) {
     stream_set_blocking($stream, false);
 }
@@ -19,10 +21,17 @@ do {
     }
 
     foreach($copyReadStream as $key => $stream){
-        echo fgets($stream);
+        $content = stream_get_contents($stream);
+        $posicaoFimHttp = strpos($content, "\r\n\r\n");
+        if($posicaoFimHttp !== false){
+            echo substr($content, $posicaoFimHttp + 4);
+        } else {
+            echo $content;
+        }
+        //echo fgets($stream);
         unset($streamList[$key]);
     }
 
 } while (!empty($streamList));
 
-echo "Li todos os arquivos" . PHP_EOL;
+echo "Li todos os streams" . PHP_EOL;
